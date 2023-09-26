52032cedb47ef07df5d28d0c46bf493761ef5800
Plumbiu <3434909403@qq.com>
2023-09-26 19:49:21 +0800

    feat: add HomeCard

---
+++ src/app/article/[id]/loading.tsx
@@ -0,0 +1,12 @@
+import { CircularProgress } from '@mui/material'
+import React from 'react'
+
+function loading() {
+  return (
+    <div>
+      <CircularProgress color="secondary" />
+    </div>
+  )
+}
+
+export default loading
