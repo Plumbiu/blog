eec1944b5aca8f09b241febf3b840369658b1639
Plumbiu <3434909403@qq.com>
2023-09-28 22:42:11 +0800

    fix: client error

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,11 +1,18 @@
+import InfoCard from '@/components/SideCard'
 import TagsCmp from '@/components/Tags'
+import Container from '@/components/ui/Container'
 import { useRequest } from '@/lib/api'
 import React from 'react'
 
 // FIXME: client component is not support async/await
 const Tags = async () => {
   const tags = await useRequest<Tag[]>('tags')
-  return <TagsCmp tags={tags} />
+  return (
+    <Container>
+      <InfoCard />
+      <TagsCmp tags={tags} />
+    </Container>
+  )
 }
 
 export default Tags
--- src/components/Tags/index.tsx
+++ src/components/Tags/index.tsx
@@ -1,9 +1,7 @@
 'use client'
-import InfoCard from '@/components/SideCard'
 import Main from '@/components/ui/Main'
 import { Badge, Chip, Typography } from '@mui/material'
 import type { FC } from 'react'
-import Container from '../ui/Container'
 
 interface Props {
   tags: Tag[]
@@ -11,33 +9,30 @@ interface Props {
 
 const TagsCmp: FC<Props> = ({ tags }) => {
   return (
-    <Container>
-      <InfoCard />
-      <Main>
-        <Typography
-          variant="h6"
-          component="div"
+    <Main>
+      <Typography
+        variant="h6"
+        component="div"
+        sx={{
+          textAlign: 'center',
+          py: 2,
+        }}
+      >
+        🎉 收录了 {tags.length} 个 tag! 🎉
+      </Typography>
+      {tags.map(({ name, count }) => (
+        <Badge
+          key={name}
+          badgeContent={count}
+          color="primary"
           sx={{
-            textAlign: 'center',
-            py: 2,
+            m: 1,
           }}
         >
-          🎉 收录了 {tags.length} 个 tag! 🎉
-        </Typography>
-        {tags.map(({ name, count }) => (
-          <Badge
-            key={name}
-            badgeContent={count}
-            color="primary"
-            sx={{
-              m: 1,
-            }}
-          >
-            <Chip variant="outlined" label={name} clickable />
-          </Badge>
-        ))}
-      </Main>
-    </Container>
+          <Chip variant="outlined" label={name} clickable />
+        </Badge>
+      ))}
+    </Main>
   )
 }
 
