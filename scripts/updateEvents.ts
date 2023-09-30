ccd169dd7e185f4deb217fef2604c144099daf64
Plumbiu <3434909403@qq.com>
2023-09-30 15:21:08 +0800

    feat: use github action update events

---
+++ scripts/updateEvents.ts
@@ -0,0 +1,13 @@
+import fsp from 'node:fs/promises'
+import { initFields, profileInfo } from '@plumbiu/github-info'
+import path from 'node:path'
+
+async function preBuild() {
+  const { eventsField } = await initFields('Plumbiu')
+  await fsp.writeFile(
+    path.join(process.cwd(), 'src', 'app', 'Plumbiu.json'),
+    JSON.stringify(await eventsField()),
+  )
+}
+
+void preBuild()
