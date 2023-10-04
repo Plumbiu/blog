a8987b764b8f6a385b87165f36440d0c857f8baf
Plumbiu <3434909403@qq.com>
2023-10-04 20:08:10 +0800

    perf: optimize PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,16 +7,20 @@ import RightCard from '@/components/ui/Container/Right'
 export const metadata: Metadata = {
   title: 'Plumbiu の 小屋',
   description: 'Welcome to my blog!',
+  themeColor: '#1976d2',
+  viewport: 'width=device-width, initial-scale=1.0',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <link rel="icon" type="image/x-icon" href="/favico.jpg"></link>
+      <link rel="icon" type="image/x-icon" sizes="32x32" href="/icons/favico-32x32.png"></link>
+      <link rel="icon" type="image/x-icon" sizes="96x96" href="/icons/favico-96x96.png"></link>
+      <link rel="icon" type="image/x-icon" sizes="128x128" href="/icons/favico-128x128.png"></link>
+      <link rel="icon" type="image/x-icon" sizes="192x192" href="/icons/favico-192x192.png"></link>
+      <link rel="icon" type="image/x-icon" sizes="256x256" href="/icons/favico-256x256.png"></link>
+      <link rel="icon" type="image/x-icon" sizes="512x512" href="/icons/favico-512x512.png"></link>
+
       <body
         style={{
           backgroundColor: '#F6F8FC',
---
+++ src/app/manifest.ts
@@ -0,0 +1,64 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Plumbiu の 小屋',
+    short_name: "Plumbiu's blog",
+    description: 'Plumbiu 的 material 风格博客',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#F6F8FC',
+    theme_color: '#1976d2',
+    prefer_related_applications: true,
+    related_applications: [
+      {
+        platform: 'play',
+        url: 'https://blog.plumbiu.top',
+        id: 'com.app.blog.plumbiu',
+      },
+      {
+        platform: 'play',
+        url: 'https://blog.plumbiu.top',
+        id: 'com.app.blog.plumbiu',
+      },
+    ],
+    icons: [
+      {
+        src: 'icons/favico-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: 'icons/favico-96x96.png',
+        sizes: '96x96',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: 'icons/favico-128x128.png',
+        sizes: '128x128',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: 'icons/favico-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: 'icons/favico-256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: 'icons/favico-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
+      },
+    ],
+  }
+}
