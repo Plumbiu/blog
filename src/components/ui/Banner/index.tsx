e9673f1af9d1535076cf29f9133dd3c5198a912e
Plumbiu <3434909403@qq.com>
2023-10-05 21:31:07 +0800

    feat: add cover for article list

--- src/components/ui/Banner/index.tsx
+++ src/components/ui/Banner/index.tsx
@@ -5,11 +5,23 @@ import './index.css'
 import Tag from '@/components/ui/Tag'
 import { AccessTimeFilled } from '@mui/icons-material'
 import { formatTime } from '@/lib/time'
+import Image from 'next/image'
 
 interface Props {
   posts: FullFrontMatter[]
   name: string
   path: string
+}
+
+const imageMap: Record<string, string> = {
+  vue: 'vue.svg',
+  rollup: 'rollup.svg',
+}
+
+function toImage(tag: string) {
+  const key = Object.keys(imageMap).find(item => item.includes(tag.toLocaleLowerCase()))
+  if (!key) return key
+  return imageMap[key]
 }
 
 const ArticleBanner: FC<Props> = ({ posts, name, path }) => {
@@ -18,25 +30,32 @@ const ArticleBanner: FC<Props> = ({ posts, name, path }) => {
     <div className="Banner-Wrap">
       {posts.map(({ id, desc, title, tags, categories, date }) => (
         <Link key={id} className="hover-a-style Banner-Link" href={'/post/' + id}>
-          <div className="Banner-Link-Title">{title}</div>
-          <div className="Banner-Date">
-            <AccessTimeFilled
-              sx={{
-                fontSize: '14px',
-                color: '#1976D2',
-                mr: '4px',
-              }}
-            />
-            <p>{formatTime(date).split(' ')[0]}</p>
-          </div>
-          <div className="Banner-Link-Desc">{desc}...</div>
-          <div>
-            {categories.map(category => (
-              <Tag key={category} text={category} outlined />
-            ))}
-            {tags.map(tag => (
-              <Tag key={tag} text={tag} />
-            ))}
+          {toImage(tags[0]) && (
+            <div className="Banner-Cover">
+              <Image width={80} height={80} alt={tags[0]} src={'/cover/' + toImage(tags[0])} />
+            </div>
+          )}
+          <div className="Banner-List">
+            <div className="Banner-Link-Title">{title}</div>
+            <div className="Banner-Date">
+              <AccessTimeFilled
+                sx={{
+                  fontSize: '14px',
+                  color: '#1976D2',
+                  mr: '4px',
+                }}
+              />
+              <p>{formatTime(date).split(' ')[0]}</p>
+            </div>
+            <div className="Banner-Link-Desc">{desc}...</div>
+            <div>
+              {categories.map(category => (
+                <Tag key={category} text={category} outlined />
+              ))}
+              {tags.map(tag => (
+                <Tag key={tag} text={tag} />
+              ))}
+            </div>
           </div>
         </Link>
       ))}
