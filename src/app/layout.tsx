74f2266dddfcf9b1ee7670c1cfbbf7043a1a199b
Plumbiu <3434909403@qq.com>
2023-10-05 22:57:27 +0800

    fix: remove icon link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <link rel="icon" type="image/x-icon" sizes="32x32" href="/icons/favico-32x32.png"></link>
-      <link rel="icon" type="image/x-icon" sizes="96x96" href="/icons/favico-96x96.png"></link>
-      <link rel="icon" type="image/x-icon" sizes="128x128" href="/icons/favico-128x128.png"></link>
-      <link rel="icon" type="image/x-icon" sizes="192x192" href="/icons/favico-192x192.png"></link>
-      <link rel="icon" type="image/x-icon" sizes="256x256" href="/icons/favico-256x256.png"></link>
-      <link rel="icon" type="image/x-icon" sizes="512x512" href="/icons/favico-512x512.png"></link>
       <body>
         <Header />
         <Container>
