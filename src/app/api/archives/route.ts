da11c18db249bc7d6aab5ece967079a7d9be968e
Plumbiu <3434909403@qq.com>
2023-10-06 21:06:55 +0800

    feat: archives limit serach params

--- src/app/api/archives/route.ts
+++ src/app/api/archives/route.ts
@@ -2,8 +2,14 @@ import { loadArchives } from '@/lib/archives'
 import { getPosts } from '@/lib/node/article'
 import { NextResponse } from 'next/server'
 
-export async function GET() {
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url)
   const posts = await getPosts()
-  const archives = loadArchives(posts)
-  return NextResponse.json(archives.sort((a, b) => Number(b.year) - Number(a.year)))
+  let archives: Archeve[] | Archeve = loadArchives(posts).sort((a, b) => Number(b.year) - Number(a.year))
+  const limit = searchParams.get('limit')
+  if (limit !== null) {
+    archives = archives[0]
+    archives.articles = archives.articles.slice(0, Number(limit))
+  }
+  return NextResponse.json(archives)
 }
