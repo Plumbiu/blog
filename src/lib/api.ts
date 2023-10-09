a0f109b7f0b2c3613bb8076d5be70d5ab10ea1ba
Plumbiu <3434909403@qq.com>
2023-10-09 21:01:11 +0800

    fix: api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,13 @@
+const queryURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/' : 'https://blog.plumbiu.top/api/'
+
 export async function useGet<T>(suffix: string) {
-  const raw = await fetch(process.env.API_URL + suffix)
+  const raw = await fetch(queryURL + suffix)
   const data: T = await raw.json()
   return data
 }
 
 export async function usePost<T>(suffix: string, payload: any) {
-  const raw = await fetch(process.env.API_URL + suffix, {
+  const raw = await fetch(queryURL + suffix, {
     method: 'POST',
     body: JSON.stringify(payload),
   })
