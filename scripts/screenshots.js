c1ba87d4894f6a228a2c5919f94afa0f89065ddb
Plumbiu <3434909403@qq.com>
2023-10-17 21:09:40 +0800

    fix: delay in screenshots

--- scripts/screenshots.js
+++ scripts/screenshots.js
@@ -1,13 +1,13 @@
 import path from 'node:path'
 import Pageres from 'pageres'
-import { readJSON } from './utils'
+import { readJSON } from './utils.js'
 
 async function screenshots() {
   const friends = await readJSON(
     path.join(process.cwd(), 'config', 'friends.json'),
   )
   for (const friend of friends) {
-    await new Pageres({ filename: friend.name })
+    await new Pageres({ delay: 2, filename: friend.name })
       .source(friend.link, ['1920x1080'], { crop: 1080 })
       .destination('public/friends/screenshots')
       .run()
