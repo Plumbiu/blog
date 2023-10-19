5e839e8e4bab49dc616c8c5e55ae8e6361c3be7c
Plumbiu <3434909403@qq.com>
2023-10-19 21:52:34 +0800

    fix: article sort error

--- src/lib/node/article.ts
+++ src/lib/node/article.ts
@@ -2,16 +2,21 @@ import fsp from 'node:fs/promises'
 import path from 'node:path'
 import parseFM from 'simple-md-front-matter'
 import { articleLimit } from '../config'
+import { perfixTime } from '../time'
+
+function datePriority(d: string) {
+  return Number(perfixTime(d).replace(/-/g, ''))
+}
 
 export async function getPosts(pagenum = 0, isLimit = false) {
   const postsPath = path.join(process.cwd(), 'posts')
   const start = pagenum * articleLimit
   const DATEREG = /([\d]{4}-[\d]+-[\d]+)/
   let rawPosts = (await fsp.readdir(postsPath)).sort((a, b) => {
-    return (
-      Number(DATEREG.exec(b)?.[0].replace(/-/g, '')) -
-      Number(DATEREG.exec(a)?.[0].replace(/-/g, ''))
-    )
+    const d1 = datePriority(DATEREG.exec(a)?.[0]!)
+    const d2 = datePriority(DATEREG.exec(b)?.[0]!)
+
+    return d2 - d1
   })
   if (isLimit) {
     rawPosts = rawPosts.slice(start, start + articleLimit)
@@ -21,7 +26,7 @@ export async function getPosts(pagenum = 0, isLimit = false) {
     const file = await fsp.readFile(path.join(postsPath, post), 'utf-8')
     const end = file.indexOf('---', 3)
     const desc = file
-      .slice(end + 3, end + 250)
+      .slice(end + 3, end + 200)
       .replace(/[#`\s-*!]/g, '')
       .replace(/\[[\w\W]*\]\(/g, ' ')
       .replace(')', ' ')
