91301f5695cc0be94525d47c4c11aaecab1eb285
Plumbiu <3434909403@qq.com>
2023-10-26 10:53:28 +0800

    fix: add metadata in Lab page

--- src/app/(Info)/lab/page.tsx
+++ src/app/(Info)/lab/page.tsx
@@ -1,22 +1,14 @@
+import type { Metadata } from 'next'
 import LabCmp from '@/components/app/Lab'
+import { name } from '~/config.json'
 
-const webDev = [
-  {
-    title: 'nextjs_tensorflow',
-    link: 'https://nextjs-tensorflow.vercel.app/',
-    desc: '使用 nextjs 和 tensorflow JavaScript API 构建的深度学习 WebUI',
-  },
-  {
-    title: 'nextjs_tensorflow',
-    link: 'https://fe-tensorflow.vercel.app/',
-    desc: '使用 vue 和 tensorflow JavaScript API 构建的深度学习 WebUI',
-  },
-]
+export const metadata: Metadata = {
+  title: `${name} | 实验室`,
+  description: `这里是 ${name} 的实验室`,
+}
 
 const Lab = () => {
-  return (
-    <LabCmp />
-  )
+  return <LabCmp />
 }
 
 export default Lab
