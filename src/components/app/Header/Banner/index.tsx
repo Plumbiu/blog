a70c90403805259c84053a5bb8a4afa1a89f5945
Plumbiu <3434909403@qq.com>
2023-11-03 20:33:30 +0800

    Update Header Banner styles and add contact info icons

--- src/components/app/Header/Banner/index.tsx
+++ src/components/app/Header/Banner/index.tsx
@@ -1,13 +1,33 @@
 import type { FC } from 'react'
 import './index.css'
+import { EmailIcon, LocationIcon, RssIcon } from '@/components/icons'
+import { location, email } from '~/config.json'
 
 interface Props {}
+
+const info = [
+  {
+    primary: 'rss',
+    icon: <RssIcon />,
+    href: '/rss.xml',
+  },
+  { primary: location, icon: <LocationIcon /> },
+  {
+    primary: email,
+    icon: <EmailIcon />,
+  },
+]
 
 const HeaderBanner: FC<Props> = ({}) => {
   return (
     <div className="Header-Banner">
       <div>
         <h1>Plumbiu の 小屋</h1>
+      </div>
+      <div className="Header-Banner-Icon">
+        {info.map(({ primary, icon }) => (
+          <div>{icon}</div>
+        ))}
       </div>
     </div>
   )
