6a02d14938f1fa8ffd96937e0c85511754cb1008
Plumbiu <3434909403@qq.com>
2023-11-03 22:14:14 +0800

    chore: remove preact

--- next.config.cjs
+++ next.config.cjs
@@ -7,18 +7,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-
-    return config
-  },
 }
 
 module.exports = nextConfig
