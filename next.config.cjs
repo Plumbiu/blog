18ce6f29334441175033330e70306aeab0cf137d
Plumbiu <3434909403@qq.com>
2023-11-03 21:51:46 +0800

    feat: try use preact

--- next.config.cjs
+++ next.config.cjs
@@ -7,6 +7,18 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { dev, isServer }) => {
+    if (!isServer) {
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
