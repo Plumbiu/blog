ecac4873228aeebecf8585da123f18b396901d50
Plumbiu <3434909403@qq.com>
2023-11-04 16:51:40 +0800

    Add border styles and update transition properties in Nav and Container components

--- src/components/app/Container/Nav/index.tsx
+++ src/components/app/Container/Nav/index.tsx
@@ -3,7 +3,6 @@
 import Link from 'next/link'
 import { type FC } from 'react'
 import './index.css'
-import { usePathname } from 'next/navigation'
 import {
   ArticleIcon,
   TravelExploreIcon,
@@ -22,25 +21,21 @@ const lists = [
     link: '/article',
     icon: <ArticleIcon />,
   },
-  { text: '开源之旅', link: '/opensource', icon: <TravelExploreIcon /> },
   { text: '朋友们', link: '/friend', icon: <PeopleIcon /> },
+  { text: '实验室', link: '/lab', icon: <LabIcon /> },
   { text: '标签', link: '/tag', icon: <TagIcon /> },
   { text: '分类', link: '/category', icon: <CategoryIcon /> },
-  { text: '实验室', link: '/lab', icon: <LabIcon /> },
+  { text: '开源之旅', link: '/opensource', icon: <TravelExploreIcon /> },
 ]
 
 const Nav: FC<Props> = ({}) => {
-  const path = usePathname()
-
   return (
     <div className="Navbar">
       {lists.map(({ link, icon, text }) => (
         <Link
           key={text}
           scroll={false}
-          className={`Navbar-Link ${
-            path.includes(link) ? 'Navbar-Link-Active' : ''
-          }`}
+          className="Navbar-Link"
           href={link + '#' + link.slice(1)}
         >
           {icon}
