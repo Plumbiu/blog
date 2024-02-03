179fecdf6959ce2ef1624d2c95b1f65642248973
Plumbiu <3434909403@qq.com>
2024-02-03 12:05:01 +0800

    feat: ui

--- src/components/app/Container/Header/index.tsx
+++ src/components/app/Container/Header/index.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import Search from './Search'
 import './index.css'
 import HeaderToggle from './Toggle'
-import HeaderMenu from './Menu'
 import { RssIcon } from '@/components/icons'
 
 const info = [
@@ -16,7 +15,6 @@ const info = [
 export default function Header() {
   return (
     <div className="Header">
-      <HeaderMenu />
       <div className="Header-Search">
         {info.map(({ primary, href, icon }) => (
           <Link key={primary} target="_blank" href={href} className="Hover">
