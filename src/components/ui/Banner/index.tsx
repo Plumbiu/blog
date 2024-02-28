dc9c07d14ae3f182bf8b4f2f06dca28efe6c9c67
Plumbiu <3434909403@qq.com>
2024-02-28 09:41:20 +0800

    fix: use a link

--- src/components/ui/Banner/index.tsx
+++ src/components/ui/Banner/index.tsx
@@ -26,13 +26,7 @@ const ArticleBanner: FC<Props> = ({ posts, name, children }) => {
   return (
     <div className="Banner">
       {posts.map(({ id, desc, title, tags, categories, date, updated }) => (
-        <Link
-          download={false}
-          prefetch={false}
-          className="Hover-Border"
-          key={id}
-          href={'/post/' + id}
-        >
+        <a className="Hover-Border" key={id} href={'/post/' + id}>
           {/* {toImage(tags[0]) && (
             <div className="Banner-Cover">
               <Image
@@ -65,7 +59,7 @@ const ArticleBanner: FC<Props> = ({ posts, name, children }) => {
               ))}
             </div>
           </div>
-        </Link>
+        </a>
       ))}
       {children}
     </div>
