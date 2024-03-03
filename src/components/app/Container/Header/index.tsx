3e6d48d17cdc07fcc61d88acac7770633c2a69b8
Plumbiu <3434909403@qq.com>
2024-03-03 18:19:37 +0800

    feat: wave

--- src/components/app/Container/Header/index.tsx
+++ src/components/app/Container/Header/index.tsx
@@ -17,24 +17,22 @@ const info = [
 
 export default function Header() {
   return (
-    <>
-      <div className="Header">
-        <HeaderMenu />
-        <div className="Header-Search">
-          {info.map(({ primary, href, icon }) => (
-            <Link key={primary} target="_blank" href={href} className="Hover">
-              {icon}
-            </Link>
-          ))}
-          <HeaderToggle />
-          <Search
-            id={process.env.APPLICATION_ID ?? ''}
-            apiKey={process.env.API_KEY ?? ''}
-            name={process.env.APPLICATION_NAME ?? ''}
-          />
-        </div>
+    <div className="Header">
+      <HeaderMenu />
+      <div className="Header-Search">
+        {info.map(({ primary, href, icon }) => (
+          <Link key={primary} target="_blank" href={href} className="Hover">
+            {icon}
+          </Link>
+        ))}
+        <HeaderToggle />
+        <Search
+          id={process.env.APPLICATION_ID ?? ''}
+          apiKey={process.env.API_KEY ?? ''}
+          name={process.env.APPLICATION_NAME ?? ''}
+        />
       </div>
       <Wave />
-    </>
+    </div>
   )
 }
