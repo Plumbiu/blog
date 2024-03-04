f1045edeaca2239a14650dda16b9bc92487603f5
Plumbiu <3434909403@qq.com>
2024-03-04 10:46:35 +0800

    feat: add tag in header title

--- src/components/app/Container/Header/Title/index.tsx
+++ src/components/app/Container/Header/Title/index.tsx
@@ -2,17 +2,42 @@
 
 import { useEffect, type FC, useState } from 'react'
 import './index.css'
+import Tag from '@/components/ui/Tag'
+import Stack from '@/components/ui/Stack'
 
 interface Props {}
 
 const HeaderTitle: FC<Props> = ({}) => {
-  const [title, setTitle] = useState('')
-
-  useEffect(() => {
-    setTitle(document.title.split('| ')?.[1] ?? document.title)
+  const [meta, setMeta] = useState({
+    title: '',
+    keywords: '',
+    categories: '',
   })
 
-  return <h1 className="Header-Title">{title}</h1>
+  useEffect(() => {
+    const metas = document.getElementsByTagName('meta') as any
+    const keywords = metas?.keywords?.content
+    const categories = metas?.category?.content
+
+    const title = document.title.split('| ')?.[1] ?? document.title
+    setMeta({
+      title,
+      keywords,
+      categories,
+    })
+  }, [])
+
+  return (
+    <div className="Header-Title-Wrapper">
+      <h1 className="Header-Title">{meta.title}</h1>
+      <div className="Header-Title-Tag">
+        <Tag text={meta.categories} filled />
+        {meta.keywords?.split(',').map((item) => (
+          <Tag key={item} text={item} filled />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default HeaderTitle
