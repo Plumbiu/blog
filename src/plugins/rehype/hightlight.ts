742bdcd738ade42a23ea93c509bf306b92530573
Plumbiu <3434909403@qq.com>
2024-10-20 11:24:33 +0800

    fix: add license

--- src/plugins/rehype/hightlight.ts
+++ src/plugins/rehype/hightlight.ts
@@ -26,12 +26,82 @@
 */
 import { visit } from 'unist-util-visit'
 import { toString } from 'hast-util-to-string'
-import rangeParser from 'parse-numeric-range'
 import type { Element, Root } from 'hast'
-import { HighlighterCore, HighlighterGeneric } from 'shiki/core'
+import { HighlighterCore } from 'shiki/core'
 import { getCodeFromProps } from '../remark/playground'
 import { isString } from '@/utils'
 
+// This code is modified based on
+// https://github.com/euank/node-parse-numeric-range/blob/master/index.js
+/*
+  LICENSE: https://github.com/euank/node-parse-numeric-range/blob/master/LICENSE
+
+  Copyright (c) 2014, Euank <euank@euank.com>
+
+  Permission to use, copy, modify, and/or distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+const RangeNumRegx = /^-?\d+$/
+const LineRegx = /^(-?\d+)(-|\.\.\.?|\u2025|\u2026|\u22EF)(-?\d+)$/
+function parsePart(s: string) {
+  let res = new Set()
+  let m
+  for (let str of s.split(',')) {
+    str = str.trim()
+    if (RangeNumRegx.test(str)) {
+      res.add(parseInt(str, 10))
+    } else if ((m = str.match(LineRegx))) {
+      let [_, lhs, sep, rhs] = m as any
+      if (lhs && rhs) {
+        lhs = parseInt(lhs)
+        rhs = parseInt(rhs)
+        const incr = lhs < rhs ? 1 : -1
+        if (sep === '-' || sep === '..' || sep === '\u2025') rhs += incr
+
+        for (let i = lhs; i !== rhs; i += incr) res.add(i)
+      }
+    }
+  }
+
+  return res
+}
+
+// This code is modified based on
+// https://github.com/timlrx/rehype-prism-plus/blob/main/src/generator.js
+/*
+  LICENSE: https://github.com/timlrx/rehype-prism-plus/blob/main/LICENSE
+
+  MIT License
+  
+  Copyright (c) 2021 Timothy
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
 const getLanguage = (className: string[]) => {
   for (const classListItem of className) {
     if (classListItem.slice(0, 9) === 'language-') {
@@ -40,10 +110,6 @@ const getLanguage = (className: string[]) => {
   }
   return 'txt'
 }
-
-/**
- * Create a closure that determines if we have to highlight the given index
- */
 const NumRangeRegx = /{([\d,-]+)}/
 const calculateLinesToHighlight = (meta: string) => {
   const parsed = NumRangeRegx.exec(meta)
@@ -51,7 +117,7 @@ const calculateLinesToHighlight = (meta: string) => {
     return () => false
   }
   const strlineNumbers = parsed[1]
-  const lineNumbers = rangeParser(strlineNumbers)
+  const lineNumbers = parsePart(strlineNumbers)
   // @ts-ignore has patched to Set<number>
   return (index: number) => lineNumbers.has(index + 1)
 }
@@ -66,9 +132,7 @@ const themeOptions = {
   light: 'vitesse-light',
   dark: 'vitesse-dark',
 }
-const rehypePrismGenerator = (
-  shiki: HighlighterCore | HighlighterGeneric<any, any>,
-) => {
+const rehypePrismGenerator = (shiki: HighlighterCore) => {
   return () => {
     return (tree: Root) => {
       visit(tree, 'element', function visitor(node, index, parent) {
