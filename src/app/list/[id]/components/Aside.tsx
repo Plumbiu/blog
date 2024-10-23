3d21f7b56d926aa1e6cb482d3dedfb9d11706f18
Plumbiu <3434909403@qq.com>
2024-10-23 17:21:51 +0800

    chore: css style

--- src/app/list/[id]/components/Aside.tsx
+++ src/app/list/[id]/components/Aside.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import styles from './Aside.module.css'
 import { FloatItem } from '../types'
+import { MenuIcon } from '@/app/components/Icons'
 
 interface ListFloatProps {
   items: [string, FloatItem[]][]
@@ -8,19 +9,24 @@ interface ListFloatProps {
 
 const ListFloat = ({ items }: ListFloatProps) => {
   return (
-    <aside className={styles.content}>
-      {items.map(([year, items]) => (
-        <div key={year} className={styles.item}>
-          <div className={styles.year}>{year}</div>
-          <ul className={styles.links}>
-            {items.map((item) => (
-              <li key={item.path}>
-                <Link href={item.path}>{item.title}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+    <aside className={styles.wrap}>
+      <div className={styles.btn}>
+        <MenuIcon />
+      </div>
+      <div className={styles.content}>
+        {items.map(([year, items]) => (
+          <div key={year} className={styles.item}>
+            <div className={styles.year}>{year}</div>
+            <ul className={styles.links}>
+              {items.map((item) => (
+                <li key={item.path}>
+                  <Link href={item.path}>{item.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </aside>
   )
 }
