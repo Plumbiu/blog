50e7cfee5f2d09996811269a7973a96cdf53e8d8
Plumbiu <3434909403@qq.com>
2024-10-24 10:22:08 +0800

    feat: style & post

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -11,7 +11,7 @@ export const noto = Inter({
   subsets: ['latin'],
   style: ['normal'],
   display: 'swap',
-  weight: ['400', '600'],
+  weight: ['400', '500', '600'],
 })
 
 export const mono = Fira_Mono({
