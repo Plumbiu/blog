a8f9e1204ce63ea78b4863bf7c7d5d59956a63f6
Plumbiu <3434909403@qq.com>
2024-12-01 16:29:59 +0800

    perf: reduce font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import ArtlistAll from './list/[id]/[pagenum]/page'
 
 function Home() {
-  return <ArtlistAll params={Promise.resolve({ id: 'blog', pagenum: 1 })} />
+  return <ArtlistAll params={Promise.resolve({ id: 'blog', pagenum: '1' })} />
 }
 
 export default Home
