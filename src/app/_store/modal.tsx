0fc37a3b6db825f061bcb91ef0c25618f0aaf555
Plumbiu <3434909403@qq.com>
2024-12-04 13:23:26 +0800

    refactor(Modal): remove hidden animation

--- src/app/_store/modal.tsx
+++ src/app/_store/modal.tsx
@@ -2,13 +2,10 @@
 
 import { createStore } from '@plumbiu/react-store'
 import React, { RefObject } from 'react'
-import modalStyle from '../_components/Modal.module.css'
 
 function preventBodyScroll(e: Event) {
   e.preventDefault()
 }
-
-let isEventAdded = false
 
 const useModalStore = createStore({
   children: null as React.ReactElement | null,
@@ -18,23 +15,16 @@ const useModalStore = createStore({
       if (!dom) {
         return
       }
-      dom.classList.add(modalStyle.hide)
-      setTimeout(() => {
-        document.body.removeEventListener('wheel', preventBodyScroll)
-        document.body.removeEventListener('touchmove', preventBodyScroll)
-        this.$set({ children: null })
-        dom.classList.remove(modalStyle.hide)
-      }, 150)
+      window.removeEventListener('popstate', this.hidden)
+      document.body.removeEventListener('wheel', preventBodyScroll)
+      document.body.removeEventListener('touchmove', preventBodyScroll)
+      this.$set({ children: null })
     } else {
       this.$set({ children: null })
     }
   },
   set(children: React.ReactElement) {
-    if (!isEventAdded) {
-      console.log(123)
-      window.addEventListener('popstate', this.hidden)
-      isEventAdded = true
-    }
+    window.addEventListener('popstate', this.hidden)
     document.body.addEventListener('wheel', preventBodyScroll, {
       passive: false,
     })
