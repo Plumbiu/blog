0cf68a50c152d033078c2278eebfeac2bec75f03
Plumbiu <3434909403@qq.com>
2024-12-20 20:29:06 +0800

    feat: preload comment

--- src/app/posts/components/Comment.tsx
+++ src/app/posts/components/Comment.tsx
@@ -18,7 +18,6 @@ function Comment() {
     element.setAttribute('data-input-position', 'bottom')
     element.setAttribute('data-theme', window.getLocalTheme())
     element.setAttribute('data-lang', 'zh-CN')
-    element.setAttribute('data-loading', 'lazy')
     element.crossOrigin = 'anonymous'
     element.async = true
     ref.current!.appendChild(element)
