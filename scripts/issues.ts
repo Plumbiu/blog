6df779810685429448c88bd5559d7ac2dafd4489
Plumbiu <3434909403@qq.com>
2025-04-05 20:08:34 +0800

    fix: issue data

--- scripts/issues.ts
+++ scripts/issues.ts
@@ -1,34 +1,48 @@
+import { getPostList, PostList } from '@/utils/node/markdown'
 import fsp from 'node:fs/promises'
+import pc from 'picocolors'
 import issueMap from '~/data/issues.json'
 import { GithubName, GithubRepoName } from '~/data/site'
 
 const update_issue = process.env.UPDATE_ISSUE === 'true'
 
-export async function createIssues() {
+export async function createIssues(posts: PostList[]) {
   const allIssues: any[] = await fetch(
     `https://api.github.com/repos/${GithubName}/${GithubRepoName}/issues`,
   ).then((res) => res.json())
   const issues = allIssues.filter((issue) =>
     issue.title.startsWith('[comment] '),
   )
-  const postIssues: string[] = []
+  let shouldUpdateLocalIssue = false
+  const issueFullMap: Record<string, number> = issueMap
+  const postsTitles = posts.map((post) => `[comment] ${post.path}`)
   for (const issue of issues) {
     const key = issue.title.replace('[comment] posts/', '')
     if (!issueMap[key]) {
-      postIssues.push(issue.title)
+      issueFullMap[key] = +issue.number
+      shouldUpdateLocalIssue = true
+    }
+  }
+  if (shouldUpdateLocalIssue) {
+    await fsp.writeFile('data/issues.json', JSON.stringify(issueFullMap))
+  }
+  const updatedIssues: string[] = []
+  const existenIssues = new Set(issues.map((issue) => issue.title))
+  for (const title of postsTitles) {
+    if (!existenIssues.has(title)) {
+      updatedIssues.push(title)
     }
   }
 
   const files = await fsp.readFile('.env.local', 'utf-8')
   const envs = files.split(/\r?\n/g)
-  let shouldUpdate = true
   for (const env of envs) {
     if (env.startsWith('GITHUB_TOKEN=')) {
       const token = env.split('=')[1].trim()
       const querylUrl = `https://api.github.com/repos/${GithubName}/${GithubRepoName}/issues`
       const issueMap: Record<string, number> = {}
       await Promise.all(
-        postIssues.map(async (title) => {
+        updatedIssues.map(async (title) => {
           await fetch(querylUrl, {
             method: 'POST',
             headers: {
@@ -48,21 +62,19 @@ export async function createIssues() {
               issueMap[title.replace('[comment] posts/', '')] = +issueNumber
             })
             .catch(() => {
-              console.log(
-                `/data/issues.json 缺少\n${pc.bgCyan(postIssues.join('\n'))}`,
-              )
+              console.log(`缺少\n${pc.bgCyan(updatedIssues.join('\n'))}`)
               console.log(pc.cyan('请手动更新 /data/issue.json'))
-              shouldUpdate = false
             })
         }),
       )
-      if (shouldUpdate) {
-        await fsp.writeFile('data/issues.json', JSON.stringify(issueMap))
-      }
     }
   }
 }
 
+async function run() {
+  const posts = await getPostList()
+  await createIssues(posts)
+}
 if (update_issue) {
-  createIssues()
+  run()
 }
