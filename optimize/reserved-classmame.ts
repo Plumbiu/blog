56445a12e0cb88649e9a829cdf0ce698cae996e9
Plumbiu <3434909403@qq.com>
2025-04-08 18:19:09 +0800

    chore: remove global cssPaths

--- optimize/reserved-classmame.ts
+++ optimize/reserved-classmame.ts
@@ -2,11 +2,10 @@ import { readFileSync } from 'node:fs'
 import { transform } from 'lightningcss'
 import { globSync } from 'fast-glob'
 
-const cssPaths = globSync('src/**/*.css', {
-  ignore: ['**/*.module.css'],
-})
-
 export default function getReservedNamesClassnames() {
+  const cssPaths = globSync('src/**/*.css', {
+    ignore: ['**/*.module.css'],
+  })
   const reversedClassNames: string[] = []
   cssPaths.forEach((path) => {
     const code = readFileSync(path)
