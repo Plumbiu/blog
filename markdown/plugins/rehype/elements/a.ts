2a555b674963e9d212b10bfc068b7eb1459dc287
Plumbiu <3434909403@qq.com>
2025-04-27 17:13:35 +0800

    fix: blockquote inline code style

--- markdown/plugins/rehype/elements/a.ts
+++ markdown/plugins/rehype/elements/a.ts
@@ -1,9 +1,10 @@
 import type { Element } from 'hast'
+import { addNodeClassName } from '../utils'
 
 export default function aElementPlugin(node: Element) {
   if (node.tagName === 'a') {
     node.properties.target = '_blank'
-    node.properties.className = 'link'
+    addNodeClassName(node, 'link')
     node.properties.rel = 'noreferrer'
   }
 }
