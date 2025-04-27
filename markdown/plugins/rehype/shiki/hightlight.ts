145439b48b4872f9c91376d524c82267ec1ac026
Plumbiu <3434909403@qq.com>
2025-04-27 22:29:36 +0800

    feat: remove text decoration

--- markdown/plugins/rehype/shiki/hightlight.ts
+++ markdown/plugins/rehype/shiki/hightlight.ts
@@ -62,31 +62,7 @@ const themeOptions = {
 }
 // This code is modified based on
 // https://github.com/timlrx/rehype-prism-plus/blob/main/src/generator.js
-/*
-  LICENSE: https://github.com/timlrx/rehype-prism-plus/blob/main/LICENSE
-
-  MIT License
-
-  Copyright (c) 2021 Timothy
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
-*/
+// LICENSE: https://github.com/timlrx/rehype-prism-plus/blob/main/LICENSE
 const rehypeShikiHighlight = () => {
   return async (tree: Root) => {
     const shiki = await getSingletonHighlighterCore(shikiOptions)
@@ -114,7 +90,11 @@ const rehypeShikiHighlight = () => {
             transformers: [
               shikiClassTransformer({
                 map: shikiMap,
-                deletedKeys: ['--shiki-dark'],
+                deletedKeys: [
+                  '--shiki-dark',
+                  'text-decoration',
+                  '--shiki-dark-text-decoration',
+                ],
               }),
               customShikiTranformer({
                 meta,
