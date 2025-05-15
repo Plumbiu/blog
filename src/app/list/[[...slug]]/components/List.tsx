feaa5cadae37749bb2b6c76ac763063c23bf58b0
Plumbiu <3434909403@qq.com>
2025-05-15 22:31:11 +0800

    chore: update style

--- src/app/list/[[...slug]]/components/List.tsx
+++ src/app/list/[[...slug]]/components/List.tsx
@@ -1,10 +1,10 @@
-import Link from 'next/link'
 import styles from './List.module.css'
 import { DESC_MAX_LEN } from '../constants'
 import { localeMap } from '~/config/locale'
 import type { PostList } from '~/markdown/types'
 import PostMeta from '@/components/layout/post-meta'
 import { cn } from '@/lib/client'
+import Title from '@/components/ui/Title'
 
 function ArtList({ posts }: { posts: PostList[] }) {
   return posts.map((post) => {
@@ -12,12 +12,12 @@ function ArtList({ posts }: { posts: PostList[] }) {
     const { title, desc } = meta
     return (
       <div key={path} className={cn('load_ani', styles.list)}>
-        <Link href={'/' + path} className={styles.title} scroll>
+        <Title href={'/' + path} className={styles.title} scroll>
           {title}
           {!!locale && (
             <div className={styles.locale}>{localeMap[locale] ?? locale}</div>
           )}
-        </Link>
+        </Title>
         <PostMeta {...post} />
         <div className={styles.desc}>
           {desc && desc.length > DESC_MAX_LEN
--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -1,13 +1,30 @@
 import type { ReactNode } from 'react'
 import styles from './Title.module.css'
 import { cn } from '@/lib/client'
+import Link from 'next/link'
 
 export default function Title({
   children,
   className,
+  href,
+  ...rest
 }: {
   children: ReactNode
   className?: string
+  href?: string
+  [key: string]: any
 }) {
-  return <div className={cn(styles.title, className)}>{children}</div>
+  const cls = cn(styles.title, className)
+  if (href) {
+    return (
+      <Link href={href} className={cls} {...rest}>
+        {children}
+      </Link>
+    )
+  }
+  return (
+    <div className={cls} {...rest}>
+      {children}
+    </div>
+  )
 }
